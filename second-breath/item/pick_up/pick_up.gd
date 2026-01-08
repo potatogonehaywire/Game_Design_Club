1c0cfd21e24916a1cdace25e40fd4c2b460bce6c
potatogonehaywire <potatogonehaywire@gmail.com>
2026-01-08 12:00:46 -0800

    Fixed drop data

--- second-breath/item/pick_up/pick_up.gd
+++ second-breath/item/pick_up/pick_up.gd
@@ -3,8 +3,10 @@ extends RigidBody3D
 @export var slot_data:SlotData
 @onready var sprite_3d: Sprite3D = $Sprite3D
 
+
 func _ready() -> void:
 	sprite_3d.texture = slot_data.item_data.texture
+
 
 func _physics_process(delta: float) -> void:
 	sprite_3d.rotate_y(delta)
