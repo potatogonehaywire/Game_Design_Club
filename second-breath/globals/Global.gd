08ad34e6828abd3f808f309c7fe6b00fc5d16383
potatogonehaywire <potatogonehaywire@gmail.com>
2026-01-12 11:48:40 -0800

    merged globals and added chest to testscene

--- second-breath/globals/Global.gd
+++ second-breath/globals/Global.gd
@@ -13,6 +13,14 @@ var enemyHitID = 0
 var enemyIsHit: bool = false
 var isProjectile = false
 
+var player
+
+func use_slot_data(slot_data: SlotData) -> void:
+	slot_data.item_data.use(player)
+
+func get_global_position() -> Vector3:
+	return player.global_position
+
 
 func _process(_delta: float) -> void:
 	if stamina < 100 && stamina > 0:
