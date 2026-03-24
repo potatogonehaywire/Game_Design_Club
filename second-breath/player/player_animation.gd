450ef6eb19630d8a33d89331539fdfcfc41046ff
potatogonehaywire <196900984+potatogonehaywire@users.noreply.github.com>
2026-03-24 12:47:22 -0700

    Added hp bar, fixed player interact and direction, made attackbox move

--- second-breath/player/player_animation.gd
+++ second-breath/player/player_animation.gd
@@ -3,5 +3,5 @@ extends Node3D
 @onready var animation_tree: AnimationTree = $"../AnimationTree"
 @onready var player : Player = get_owner()
 
-func _physics_process(delta: float) -> void:
-	animation_tree.set("parameters/walking/blend_position", Vector2(player.velocity.x, player.velocity.z).normalized())
+func _physics_process(_delta: float) -> void:
+	animation_tree.set("parameters/walking/blend_position", Vector2(player.direction.x, player.direction.z))
